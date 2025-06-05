b11c3849cc9b094507b81821e197655f28f91438
Joshua Tracey <joshua.tracey08@gmail.com>
2025-06-05 22:19:09 +0100

    chore: add better nester json payload tests

--- tests/payload.rs
+++ tests/payload.rs
@@ -4,6 +4,9 @@ mod tests {
     use serde_json::Value;
     use std::collections::HashMap;
 
+    #[cfg(feature = "serde_json_ctx")]
+    use serde_json_ctx::ValueExt;
+
     #[cfg(feature = "logging")]
     use scribe_rust::Logger;
     #[cfg(feature = "logging")]
@@ -65,5 +68,65 @@ mod tests {
         let res1 = evaluator.evaluate(expr1).unwrap();
 
         assert_eq!(res1, Value::Bool(true));
+    }
+
+    #[cfg(feature = "serde_json_ctx")]
+    #[test]
+    fn test_json_payload_eval_with_serde_json_ctx() {
+        let mut context: serde_json::Map<String, Value> = serde_json::Map::new();
+
+        context.insert("event".to_string(), serde_json::Value::Null);
+        context.insert(
+            "send_email".to_string(),
+            serde_json::json!({
+                "status": "success",
+                "payload": null,
+                "satcom": {
+                    "id":  12345.0,
+                    "name": "Test Satellite"
+                }
+            }),
+        );
+
+        #[cfg(feature = "logging")]
+        let logger = Arc::new(Logger::default());
+        let evaluator = Evaluator::new(
+            context.clone().into_iter().collect(),
+            #[cfg(feature = "logging")]
+            logger,
+        );
+
+        let expr1 = "event.payload === null";
+        let res1 = evaluator.evaluate(expr1).unwrap();
+        assert_eq!(res1, serde_json::Value::Bool(true));
+        let expr2 = "send_email.status === \"success\"";
+        let res2 = evaluator.evaluate(expr2).unwrap();
+        assert_eq!(res2, serde_json::Value::Bool(true));
+        let expr3 = "send_email.satcom.id === 12345.0";
+        let res3 = evaluator.evaluate(expr3).unwrap();
+        assert_eq!(res3, serde_json::Value::Bool(true));
+        let expr4 = "send_email.satcom.name === \"Test Satellite\"";
+        let res4 = evaluator.evaluate(expr4).unwrap();
+        assert_eq!(res4, serde_json::Value::Bool(true));
+        let expr5 = "send_email.payload === null";
+        let res5 = evaluator.evaluate(expr5).unwrap();
+        assert_eq!(res5, serde_json::Value::Bool(true));
+        let expr6 = "send_email.satcom === null";
+        let res6 = evaluator.evaluate(expr6).unwrap();
+        assert_eq!(res6, serde_json::Value::Bool(false));
+
+        let expr7 = "send_email.satcom.id === 12345"; // Testing with integer comparison to float
+                                                      // rust will coerce the float to an integer
+                                                      // cus javascript coerces the float to an integer,
+                                                      // as no decimal places are present so they
+                                                      // are equal.
+        let res7 = evaluator.evaluate(expr3).unwrap();
+        assert_eq!(res7, serde_json::Value::Bool(true));
+
+        let expr8 = "send_email.satcom.id === 12345.1"; // Testing with float comparison with
+                                                        // decimal places this will be false
+                                                        // as the float is not equal to the integer
+        let res8 = evaluator.evaluate(expr8).unwrap();
+        assert_eq!(res8, serde_json::Value::Bool(false));
     }
 }
